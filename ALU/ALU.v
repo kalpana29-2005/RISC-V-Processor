b2fb2a2684cb55307051f3d466a6549a426e74e6
Kalpana_Bannad <gouribannad@gmail.com>
2026-07-25 12:03:49 +0530

    Update ALU.v

--- ALU/ALU.v
+++ ALU/ALU.v
@@ -1,4 +1,4 @@
-/*
+/**
 ALU module, which takes two operands of size 32-bits each and a 4-bit ALU_control as input.
 Operation is performed on the basis of ALU_control value and output is 32-bit ALU_result. 
 If the ALU_result is zero, a ZERO FLAG is set.
