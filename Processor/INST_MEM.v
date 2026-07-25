ad5ad4b3d39d816e8819c06fc2ac5a3729ff6ab3
Kalpana_Bannad <gouribannad@gmail.com>
2026-07-25 12:11:44 +0530

    Fix formatting in INST_MEM.v comments

--- Processor/INST_MEM.v
+++ Processor/INST_MEM.v
@@ -1,7 +1,7 @@
 /* 
 Instruction memory takes in two inputs: A 32-bit Program counter and a 1-bit reset. 
 The memory is initialized when reset is 1.
-When reset is set to 0, Based on the value of PC, corresponding 32-bit Instruction code is output
+When reset is set to 0,  Based on the value of PC, corresponding 32-bit Instruction code is output
 */
 module INST_MEM(
     input [31:0] PC,
